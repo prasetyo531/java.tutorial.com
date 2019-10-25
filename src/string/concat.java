42f5986ba13c28d8c0cc2484be4c33de11feff13
FDN-Prasetyo <prasetyo531@gmail.com>
2019-10-25 17:06:40 +0700

    string

---
+++ src/string/concat.java
@@ -0,0 +1,16 @@
+package string;
+
+public class concat {
+
+    public static void main(String[] args) {
+
+        concat1();
+    }
+
+    public static void concat1(){
+        String str1 = "Welcome";
+        String str2 = "Home";
+
+        System.out.println(str1.concat(str2));
+    }
+}
---
+++ src/string/escapeString.java
@@ -0,0 +1,14 @@
+package string;
+
+public class escapeString {
+
+    public static void main(String[] args){
+
+        String text = "\"This text is one tab in.\"";
+        String text2 = "\t\"This text is one tab in.\"";
+
+        System.out.println(text);
+        System.out.println(text2);
+
+    }
+}
