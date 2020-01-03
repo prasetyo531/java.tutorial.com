24d7ff964dee8319f6408ee1438f56c45ee6fe73
FDN-Prasetyo <prasetyo531@gmail.com>
2020-01-03 13:45:59 +0700

    accessModifier

--- src/c1AccessModifier/child.java
+++ src/c1AccessModifier/child.java
@@ -1,18 +1,18 @@
-//package chapter1;
-//
-//public class child extends parent {
-//
-//    public static void memberChild1(){
-//
-//        setRencana2("private ngga akan bisa");
-//        System.out.println(getRencana2());
-//
-//        setRencana3("protected masih bisa");
-//        System.out.println(getRencana3());
-//    }
-//
-//    public static void main(String[] args){
-//
-//        memberChild1();
-//    }
-//}
+package chapter1;
+
+public class child extends parent {
+
+    public static void memberChild1(){
+
+        setRencana2("private ngga akan bisa");
+        System.out.println(getRencana2());
+
+        setRencana3("protected masih bisa");
+        System.out.println(getRencana3());
+    }
+
+    public static void main(String[] args){
+
+        memberChild1();
+    }
+}
