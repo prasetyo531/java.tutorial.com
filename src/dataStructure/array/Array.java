bd02fe5a9a6bbabf73e49fa4b998758119a597b2
prasetyo <prasetyo531@gmail.com>
2025-04-20 23:33:33 +0700

    adjust current logic following leetcode

--- src/dataStructure/array/Array.java
+++ src/dataStructure/array/Array.java
@@ -1,20 +1,16 @@
-package array;
+package dataStructure.array;
 
 public class Array {
 
-    public static void main(String[] args) {
-        int[] intArray = new int[7];
-
-        intArray[0] = 20;
-        intArray[1] = 35;
-        intArray[2] = -15;
-        intArray[3] = 7;
-        intArray[4] =55;
-        intArray[5] = 1;
-        intArray[6] = -22;
-
+    public void printArray(int[] intArray) {
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
+    }
+
+    public static void main(String[] args) {
+        Array array = new Array();
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+        array.printArray(arr);
     }
 }
--- src/dataStructure/array/FindArrayIndex.java
+++ src/dataStructure/array/FindArrayIndex.java
@@ -1,28 +1,26 @@
-package array;
+package dataStructure.array;
 
 public class FindArrayIndex {
 
-    public static void main(String[] args) {
-        //array size
-        int[] intArray = new int[7];
-
-        intArray[0] = 20;
-        intArray[1] = 35;
-        intArray[2] = -15;
-        intArray[3] = 7;
-        intArray[4] =55;
-        intArray[5] = 1;
-        intArray[6] = -22;
-
-        //find array index
-        int index = -1;                                     //1. array start from 0
-        for (int i = 0; i < intArray.length; i++) {         //2. for condition: from zero to length array
-            if (intArray[i] == 1) {                         //3. if condition: variable array and values you need to find
-                index = i;                                  //4. initialize index same as i
-                break;
+    public int findElement(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i; // Return the index of the target
             }
         }
+        return -1; // Return -1 if the target is not found
+    }
 
-        System.out.println("index = " + index);
+    public static void main(String[] args) {
+        FindArrayIndex solution = new FindArrayIndex();
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+        int target = 7; // Change this to the number you want to find
+
+        int index = solution.findElement(arr, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index + ".");
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
     }
 }
